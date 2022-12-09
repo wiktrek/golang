afb229e9bc5dee38b58feb0ef3869098922ffadf
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-12-09 22:23:36 +0100

    crudapi first commit

---
+++ crudapi/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	// "math/rand"
+	"net/http"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+type Movie struct{
+ ID string `json"id"`
+ Isbn string `json:"isbn"` 
+ Title string `json:"title"`
+Director *Director `json:"director"`
+ } 
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+} 
+var movies []Movie
+func getMovies(w http.ResponseWriter, r*http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+func deleteMovie(w http.ResponseWriter, r*http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{ Firstname:"John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438228", Title: "Movie Two", Director: &Director{ Firstname:"Joe", Lastname: "Doe"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+fmt.Printf("Starting server on :8080 \n")
+log.Fatal(http.ListenAndServe(":8080", r))
+}
