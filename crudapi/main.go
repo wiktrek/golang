a83e1108f4825ad7bac83a9753bc61f40e0608e5
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-12-09 22:34:09 +0100

    crudapi second commit

--- crudapi/main.go
+++ crudapi/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gorilla/mux"
 )
 type Movie struct{
- ID string `json"id"`
+ ID string `json:"id"`
  Isbn string `json:"isbn"` 
  Title string `json:"title"`
 Director *Director `json:"director"`
@@ -37,7 +37,31 @@ func deleteMovie(w http.ResponseWriter, r*http.Request) {
 
 	}
 }
-
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	parms := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == parms["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
+func updateMovie(w http.ResponseWriter, r *http.Request) {}
 func main() {
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{ Firstname:"John", Lastname: "Doe"}})
--- webserver/main.go
+++ webserver/main.go
@@ -36,8 +36,8 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("Starting server at port 8000\n")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
